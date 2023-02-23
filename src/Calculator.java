3790bb0065a710d3877bd3b962b59ff1a6551b92
Conwq <serega.patseev@gmail.com>
2023-02-23 15:16:41 +0300

    Add arabic numeral method

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,65 +4,84 @@ import java.util.Scanner;
 
 public class Calculator {
     private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         String value = scanner.nextLine();
-        String [] strings = calculator.enteringValue(value);
+        String[] strings = calculator.enteringValue(value);
         calculator.expression(strings);
 
     }
 
-    public void expression(String [] strings){
-        int firstNumb = 0;
-        int secondNumb = 0;
-        String firstRomNum = null;
-        String secondRomNum = null;
-        try {
-            firstNumb = Integer.parseInt(strings[0]);
-            secondNumb = Integer.parseInt(strings[2]);
-        }
-        catch (Exception e){
-            firstNumb = 0;
-            secondNumb = 0; //не обязательно
-            firstRomNum = strings[0];
-            secondRomNum = strings [2];
-        }
+    public void expression(String[] strings) {
+
+        String firstNumb = strings[0];
         String operator = strings[1];
-        int value = ppp(firstNumb, operator, secondNumb);
-        String valueRom = ppp(firstRomNum, operator, secondRomNum);
+        String secondNumb = strings[2];
+        String answer = null;
+        try {
+            answer = withInt(firstNumb, operator, secondNumb);
+        } catch (Exception e) {
+            withString(firstNumb, operator, secondNumb);
+        }
+    }
+
+    public String withInt(String firstNum, String operator, String secondNum) {
+        int numbOne = Integer.parseInt(firstNum);
+        int numbTwo = Integer.parseInt(firstNum);
+        switch (operator) {
+            case "+":
+                return String.valueOf(numbOne + numbTwo);
+            case "-":
+                return String.valueOf(numbOne - numbTwo);
+            case "/":
+                return String.valueOf(numbOne / numbTwo);
+            case "*":
+                return String.valueOf(numbOne * numbTwo);
+            default:
+                return null;
+        }
+
+    }
+
+    public void withString(String firstNum, String operator, String secondNum) {
+        String answer = null;
+
     }
 
 
-
-
-
-    public String ppp(String firstRomNum, String operator, String secondRomNum){
-        return null;
-    }
-    public int ppp(int firstNumb, String operator, int secondNumb){
-        if (firstNumb == 0 || secondNumb == 0){
+    public int ppp(int firstNumb, String operator, int secondNumb) {
+        if (firstNumb == 0 || secondNumb == 0) {
             return 0;
         }
-        switch(operator){
-            case "+": return firstNumb + secondNumb;
-            case "-": return firstNumb - secondNumb;
-            case "/": return firstNumb / secondNumb;
-            case "*": return firstNumb * secondNumb;
-            default: return 0;
+        switch (operator) {
+            case "+":
+                return firstNumb + secondNumb;
+            case "-":
+                return firstNumb - secondNumb;
+            case "/":
+                return firstNumb / secondNumb;
+            case "*":
+                return firstNumb * secondNumb;
+            default:
+                return 0;
         }
     }
-    public String [] enteringValue(String value){
-        String [] strings = value.split(" ");
+
+
+    public String[] enteringValue(String value) {
+        String[] strings = value.split(" ");
         checkArrayStrings(strings);
         return strings;
     }
-    public void checkArrayStrings(String [] strings) {
+
+
+    public void checkArrayStrings(String[] strings) {
         try {
             if (strings.length > 3) {
                 throw new Exception();
             }
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
