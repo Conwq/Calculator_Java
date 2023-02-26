e2f242e57e9392313d635bd4ca007e4298d7fedd
Conwq <serega.patseev@gmail.com>
2023-02-26 21:01:34 +0300

    Added method main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@ import java.util.*;
 
 public class Main {
 
+    public static void main(String[] args) throws Exception {
+        String result = "1 + 2";
+//        result = "VI / III";
+//        result = "I - II";
+//        result = "I + 1";
+//        result = "1";
+//        result = "1 + 2 + 3";
+        System.out.println(Main.calc(result));
+    }
+
     public static String calc(String input) throws Exception {
         String result = null;
         Main calculator = new Main(); //создаем объект калькулятор
